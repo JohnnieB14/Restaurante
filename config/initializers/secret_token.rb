5ee31b959f9a520225ca58c7a5c0e854b5bde5b3
JohnnieB14 <dworm.2010@gmail.com>
2012-10-01 19:01:33 -0500

    OTB

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Restaurante::Application.config.secret_token = 'dcdf457a268f7d74a5a5707b6be4d17e35b7c08ee8c30ae5a8dec8b536a3c28c1ce724bf6799ab1afff8451a839254fe0259e2397409fc24374555423cd0f5e0'
