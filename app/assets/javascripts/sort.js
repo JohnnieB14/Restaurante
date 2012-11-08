74855b89b98f9fdee77f812d8f69189d890ba6ea
JohnnieB14 <dworm.2010@gmail.com>
2012-11-07 23:56:52 -0600

    Tablesorter

---
+++ app/assets/javascripts/sort.js
@@ -0,0 +1,77 @@
+$(document).ready(function() 
+{ 
+	$("#ca_table").tablesorter({
+		headers: {
+			3: {
+				sorter: false
+			},
+			4: {
+				sorter: false
+			}
+		}
+	});
+	$("#cl_table").tablesorter({
+		headers: {
+			4: {
+				sorter: false
+			}
+		}
+	});
+	$("#co_table").tablesorter({
+		headers: {
+			4: {
+				sorter: false
+			},
+			5: {
+				sorter: false
+			},
+			6: {
+				sorter: false
+			}
+		}
+	});
+	$("#or_table").tablesorter({
+		headers: {
+			4: {
+				sorter: false
+			},
+			5: {
+				sorter: false
+			},
+			6: {
+				sorter: false
+			}
+		}
+	});
+	$("#ta_table").tablesorter({
+		headers: {
+			3: {
+				sorter: false
+			},
+			4: {
+				sorter: false
+			}
+		}
+	});
+	$("#us_table").tablesorter({
+		headers: {
+			5: {
+				sorter: false
+			},
+			6: {
+				sorter: false
+			}
+		}
+	});
+	$("#wa_table").tablesorter({
+		headers: {
+			3: {
+				sorter: false
+			},
+			4: {
+				sorter: false
+			}
+		}
+	}); 
+} 
+);
